17b94bb865f3eaa1986e3422443857c07bf4839a
sunqkd <sunzhaoqkd@163.com>
2018-05-31 23:38:39 +0800

    '样式绑定'

--- src/app/bind/bind.component.ts
+++ src/app/bind/bind.component.ts
@@ -7,12 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindComponent implements OnInit {
 
-	constructor() { }
-	imgUrl: string = "http://placehold.it/400x220"
-	asd:boolean = true
-	ngOnInit() {
-
+	constructor() { 
+		setTimeout(() => {
+			this.divClass = "a b c";
+		}, 2000);
 	}
+	imgUrl:string = "http://placehold.it/400x220"
+	asd:boolean = false
+	size:number = 2
+	divClass:string;
+	isBig:boolean = true
+	changeClass:any = {
+		a:false,
+		b:true,
+		c:false
+	};
+	isDev:boolean = false;
+
+
+	isStyle:any = {
+		color: 'blue',
+		background: 'orange'
+	}
+	ngOnInit() {
+		
+	}
+
 	doOnInput(event:any) {
 		console.log(event.target.value) // dom 属性
 		console.log(event.target.getAttribute('value')) // html属性
