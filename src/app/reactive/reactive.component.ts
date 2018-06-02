beb3793a941766f09bb1b5b6a4f59a9dd3d5cc71
sunqkd <sunzhaoqkd@163.com>
2018-06-02 23:24:28 +0800

    'update'

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,6 +1,9 @@
 // 响应式编程
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import 'rxjs/add/observable/from'
+
 @Component({
 	selector: 'app-reactive',
 	templateUrl: './reactive.component.html',
@@ -8,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-	constructor() { }
+/*流做的三件事:
+1、from（[1,2,3,4]）
+发射下一个元素: 元素可以是任何东西类型 => 取决于流的源头 本例为数字类型的数组
+fromEvent()  流的源头发射的就是一个事件
+2、流可以抛一个异常
+3、发射一个信号告诉观察者流已经结束
 
+观察者可以定义三个方法处理 流
+1、处理流中发射出来的元素
+2、处理流中的异常（可选）
+3、在流结束的时候被调用（可选）
+*/
+
+	constructor() {
+		Observable.from([1, 2, 3, 4]) // 创建一个数组流 （被观察者）
+			.filter(e => e % 2 == 0) // 过滤偶数
+			.map(e => e * e) // 映射流
+			.subscribe( // 订阅 （观察者）
+				e => console.log(e),
+				err => console.error(err), // 可选
+				() => console.log("流结束了") // 可选
+			)
+	}
+
+	onKey(event){
+		console.log(event.target.value)
+		// event.target 指向发射事件的HTML元素
+	}
+
+	onKeyFilder(event){
+		console.log(event)
+	}
 	ngOnInit() {
+
 	}
 
 }
