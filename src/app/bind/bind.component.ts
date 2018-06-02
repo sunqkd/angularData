9234161e2b7aaaa844efc6bd65205c337acf0c58
sunqkd <sunzhaoqkd@163.com>
2018-06-02 11:18:17 +0800

    'update'

--- src/app/bind/bind.component.ts
+++ src/app/bind/bind.component.ts
@@ -7,33 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindComponent implements OnInit {
 
-	constructor() { 
+	constructor() {
 		setTimeout(() => {
 			this.divClass = "a b c";
 		}, 2000);
 	}
-	imgUrl:string = "http://placehold.it/400x220"
-	asd:boolean = false
-	size:number = 2
-	divClass:string;
-	isBig:boolean = true
-	changeClass:any = {
-		a:false,
-		b:true,
-		c:false
+	imgUrl: string = "http://placehold.it/400x220"
+	asd: boolean = true
+	size: number = 2
+	divClass: string;
+	isBig: boolean = true
+	changeClass: any = {
+		a: false,
+		b: true,
+		c: false
 	};
-	isDev:boolean = false;
+	isDev: boolean = false;
 
 
-	isStyle:any = {
+	isStyle: any = {
 		color: 'blue',
 		background: 'orange'
 	}
 	ngOnInit() {
-		
-	}
 
-	doOnInput(event:any) {
+	}
+	changeasd() {
+		this.asd = !this.asd
+	}
+	doOnInput(event: any) {
 		console.log(event.target.value) // dom 属性
 		console.log(event.target.getAttribute('value')) // html属性
 	}
